e2f101b7fd15ce0106d5c7ccf6d8bd24c6dab709
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2021-07-07 23:38:49 -0300

    feat:Component CardGame

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const $root = document.querySelector("#root")
+import "./src/styles/settings/colors.css";
+import "./src/styles/tools/NessBorder/styles.css";
+import "./src/styles/elements/base.css";
+import CardGame from "./src/components/Card";
 
-$root.textContent = "Hello DOM"
+const $root = document.querySelector("#root");
+const $htmlCard = CardGame();
+
+$root.insertAdjacentHTML("beforeend", $htmlCard);
---
+++ src/components/Card/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+function CardGame() {
+  return /*html*/ `
+  <figure class="card-game ness-border">
+    <img src="images/alura-pixel.png" alt="Logo da Alura" class="icon"/>
+  </figure>`;
+}
+
+export default CardGame;
