3ba1cb7c9ebcc5a22ee4a04553c0f702108dab84
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2021-07-12 01:31:40 -0300

    :art: Adding new folders of css

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/tools/NessBorder/styles.css";
+import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 import CardGame from "./src/components/Card";
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,9 +2,11 @@ import "./styles.css";
 
 function CardGame() {
   return /*html*/ `
-  <figure class="card-game ness-border">
-    <img src="images/alura-pixel.png" alt="Logo da Alura" class="icon"/>
-  </figure>`;
+    <article>
+      <figure class="card-game ness-border">
+        <img src="images/alura-pixel.png" alt="Logo da Alura" class="icon"/>
+      </figure>
+    </article>`;
 }
 
 export default CardGame;
